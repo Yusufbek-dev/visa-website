ee7510cdd153e3447ff0553cc54afaab592e2e29
yusufbek-dev <yusufbek.dev@gmail.com>
2023-12-05 00:14:47 +0500

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheHeader />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheHeader
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Jost, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+ul {
+  list-style: none;
+}
+html {
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,53 @@
+<template>
+    <header>
+        <div class="logo">
+            <a href="#">VISA4UZB</a>
+        </div>
+        <ul class="nav-list">
+            <li class="nav-list__item">
+                <a href="#">Bosh Sahifa</a>
+            </li>
+            <li class="nav-list__item">
+                <a href="#">Biz bilan bog'lanish</a>
+            </li>
+            <li class="nav-list__item">
+                <a href="#">Bizning Hizmatlar</a>
+            </li>
+            <li class="nav-list__item">
+                <a href="#">Savol yo'llash</a>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+
+</script>
+
+<style scoped>
+header {
+    background-color: antiquewhite;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 40px;
+    /* box-shadow: 0px 0px 10px 0px gray; */
+}
+.nav-list {
+    display: flex;
+    justify-content: space-between;
+    
+}
+.nav-list__item {
+    margin-left: 35px;
+    font-size: 1.4rem;
+}
+a {
+    color: rgb(42, 40, 40);
+    text-decoration:none;
+
+}
+</style>
